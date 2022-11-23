24ec0f38105ce8f96e870e93fe3e7650c60b3068
Ismet1999 <kadyrovismet1999@gmail.com>
2022-11-24 03:41:31 +0500

    fix router path

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const router = new Router();
 const authController = require("../controllers/authController.js");
 const { refreshToken } = require("../middlewares/authMiddleware.js");
 
-router.post("/", authController.login);
+router.post("/login", authController.login);
 router.post("/refresh", refreshToken, authController.refresh);
 
 module.exports = router;
